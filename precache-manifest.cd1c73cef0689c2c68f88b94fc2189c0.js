6b78da34742c6724ee316a1c8e28b6461db4abda
Matthew Holloway <matthew@springload.co.nz>
2020-06-03 01:44:16 +1200

    Updates

--- precache-manifest.cd1c73cef0689c2c68f88b94fc2189c0.js
+++ precache-manifest.cd1c73cef0689c2c68f88b94fc2189c0.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "1490490ac2e7fda0d0d430512484033e",
+    "revision": "94a41701233008149ec37f9da7e3c910",
     "url": "/metacomponent/index.html"
   },
   {
-    "revision": "a35eec83058af84ab458",
+    "revision": "2724990d893baa983235",
     "url": "/metacomponent/static/css/main.ab41d9dc.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/metacomponent/static/js/2.78f2f869.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "a35eec83058af84ab458",
-    "url": "/metacomponent/static/js/main.0fc803ac.chunk.js"
+    "revision": "2724990d893baa983235",
+    "url": "/metacomponent/static/js/main.0fb6cb09.chunk.js"
   },
   {
     "revision": "815089547c9ab2437056",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/metacomponent/precache-manifest.e3f15b14a7d704570ea93b11a2db13c7.js"
+  "/metacomponent/precache-manifest.cd1c73cef0689c2c68f88b94fc2189c0.js"
 );
 
 self.addEventListener('message', (event) => {
